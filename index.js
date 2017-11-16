2ded63fa95ea6bb856bdc78069944589241f76e0
TheAdnan <adnankicin92@gmail.com>
2017-11-16 11:49:15 +0100

    switched to pinned tabs instead of popups

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+var open = false;
+var tab_id = 0;
 
-browser.browserAction.onClicked.addListener(function(tab){
-	browser.browserAction.setPopup({popup: "options/index.html"});
-});
+function openSonance() {
+	if(open == false){
+		var creating = browser.tabs.create({
+	     "url": "/sonance/index.html",
+	     "pinned": true
+	   });
+	   creating.then(onCreated, onError);
+	}
+	else{
+		var removing = browser.tabs.remove(tab_id);
+		removing.then(onRemoved, onError);
+	}
+   
+}
 
+browser.browserAction.onClicked.addListener(openSonance);
 
+function onRemoved() {
+	unset();
+}
 
+function onError(error) {
+	unset();
+}
 
+function onCreated(tab) {
+  tab_id = tab.id;
+  open = true;
+}
+
+function unset(){
+	tab_id = 0;
+	open = false;
+}
